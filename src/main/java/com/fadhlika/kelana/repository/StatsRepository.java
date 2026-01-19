30a35e8d25dd891331eb35164f279409d3d98398
Muhammad Fadhlika <git@fadhlika.com>
2026-01-19 12:22:49 +0700

    fix geocode comparison query

--- src/main/java/com/fadhlika/kelana/repository/StatsRepository.java
+++ src/main/java/com/fadhlika/kelana/repository/StatsRepository.java
@@ -1,41 +1,47 @@
 package com.fadhlika.kelana.repository;
 
+import com.fadhlika.kelana.dto.Stats;
 import java.sql.ResultSet;
 import java.time.OffsetDateTime;
 import java.time.ZonedDateTime;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
-import com.fadhlika.kelana.dto.Stats;
-
 @Repository
 public class StatsRepository {
+
     @Autowired
     private JdbcClient jdbClient;
 
-    private final RowMapper<Stats> rowMapper = (ResultSet rs, int rowNum) -> new Stats(
+    private final RowMapper<Stats> rowMapper = (ResultSet rs, int rowNum) ->
+        new Stats(
             rs.getInt("total_points"),
             rs.getInt("total_reverse_geocoded_points"),
             rs.getInt("total_cities_visited"),
             rs.getInt("total_countries_visited"),
-            rs.getObject("last_point_timestamp", OffsetDateTime.class).toZonedDateTime());
+            rs
+                .getObject("last_point_timestamp", OffsetDateTime.class)
+                .toZonedDateTime()
+        );
 
     public Stats getStats(int userId) {
-        return jdbClient.sql("""
+        return jdbClient
+            .sql(
+                """
                     SELECT
                         COUNT(1) AS total_points,
-                        SUM(geocode != jsonb('null')) AS total_reverse_geocoded_points,
-                        COUNT(DISTINCT geocode->'$.features[0].properties.city') AS total_cities_visited,
-                        COUNT(DISTINCT geocode->'$.features[0].properties.country') AS total_countries_visited,
+                        COUNT(CASE WHEN geocode != 'null' THEN 1 END) AS total_reverse_geocoded_points,
+                        COUNT(DISTINCT geocode->'features'->0->'properties'->>'city') AS total_cities_visited,
+                        COUNT(DISTINCT geocode->'features'->0->'properties'->>'country') AS total_countries_visited,
                         MAX(timestamp) AS last_point_timestamp
                     FROM location
                     WHERE user_id = ?
-                """)
-                .param(userId)
-                .query(rowMapper)
-                .single();
+                """
+            )
+            .param(userId)
+            .query(rowMapper)
+            .single();
     }
 }
